264f9090e90976a422da4010c39cff88eb63dd0a
ajaberi <adnan.ajaberi@gmail.com>
2023-05-30 13:46:40 +0300

    base program

--- program/main.go
+++ program/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) != 2 {
 		fmt.Println("Usage: go run . <text>")
 		return
 	}
@@ -20,25 +20,64 @@ func main() {
 	}
 	defer file.Close()
 
-	str := strings.TrimSpace(os.Args[1])
+	str := os.Args[1]
+
+	for _,letter := range str {
+		if letter < ' ' || letter > '~' {
+			fmt.Println("Use english chars only")
+			os.Exit(0)
+		}
+	}
 	asciiArray := make([]int, len(str))
 
 	for i, char := range str {
 		asciiArray[i] = int(char) - 32
 	}
 
-	res := make([]string, 8)
-	for i := 0; i < 8; i++ {
-		_, err = file.Seek(0, 0) // Reset file cursor to the beginning
+	multipleNewLines := false
+	var lastcharacter rune
+	for _, character := range str {
+		if character == 'n' && lastcharacter == '\\' {
+			multipleNewLines = true
+		}
+		lastcharacter = character
+	}
+
+	if multipleNewLines {
+		words := strings.Split(str, "\\n")
+
+		for _, currentWord := range words {
+			for i := 0; i < 8; i++ {
+		
+				for _, currentLetter := range currentWord {
+					_, err = file.Seek(0, 0) // Reset file cursor to the beginning
+				if err != nil {
+					fmt.Println("Error seeking file:", err)
+					return
+				}
+		
+					line := picasso.ReadLine(file, 2+(int(currentLetter)-32)*9+i)
+					fmt.Print(line)
+				}
+				fmt.Println()
+			}
+			
+		}
+	} else {
+		for i := 0; i < 8; i++ {
+		
+		for _, currentLetter := range str {
+			_, err = file.Seek(0, 0) // Reset file cursor to the beginning
 		if err != nil {
 			fmt.Println("Error seeking file:", err)
 			return
 		}
 
-		for _, ascii := range asciiArray {
-			line := picasso.ReadLine(file, 2+ascii*9+i)
-			res[i] += line
+			line := picasso.ReadLine(file, 2+(int(currentLetter)-32)*9+i)
+			fmt.Print(line)
 		}
-		fmt.Println(res[i])
+		fmt.Println()
+	}
 	}
 }
+

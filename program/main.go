aa5c361ce1f40f832b53443fea2d0625368087c6
ajaberi <adnan.ajaberi@gmail.com>
2023-05-30 15:02:54 +0300

    fully works as needed

--- program/main.go
+++ program/main.go
@@ -22,62 +22,34 @@ func main() {
 
 	str := os.Args[1]
 
-	for _,letter := range str {
+	for _, letter := range str {
 		if letter < ' ' || letter > '~' {
-			fmt.Println("Use english chars only")
+			fmt.Println("Use English chars only")
 			os.Exit(0)
 		}
 	}
-	asciiArray := make([]int, len(str))
 
-	for i, char := range str {
-		asciiArray[i] = int(char) - 32
-	}
+	words := strings.Split(str, "\\n")
 
-	multipleNewLines := false
-	var lastcharacter rune
-	for _, character := range str {
-		if character == 'n' && lastcharacter == '\\' {
-			multipleNewLines = true
+	for current, currentWord := range words {
+		if words[current] == "" {
+			fmt.Println()
+			continue
 		}
-		lastcharacter = character
-	}
 
-	if multipleNewLines {
-		words := strings.Split(str, "\\n")
+		for i := 0; i < 8; i++ {
 
-		for _, currentWord := range words {
-			for i := 0; i < 8; i++ {
-		
-				for _, currentLetter := range currentWord {
-					_, err = file.Seek(0, 0) // Reset file cursor to the beginning
+			for _, currentLetter := range currentWord {
+				_, err = file.Seek(0, 0) // Reset file cursor to the beginning
 				if err != nil {
 					fmt.Println("Error seeking file:", err)
 					return
 				}
-		
-					line := picasso.ReadLine(file, 2+(int(currentLetter)-32)*9+i)
-					fmt.Print(line)
-				}
-				fmt.Println()
-			}
-			
-		}
-	} else {
-		for i := 0; i < 8; i++ {
-		
-		for _, currentLetter := range str {
-			_, err = file.Seek(0, 0) // Reset file cursor to the beginning
-		if err != nil {
-			fmt.Println("Error seeking file:", err)
-			return
-		}
 
-			line := picasso.ReadLine(file, 2+(int(currentLetter)-32)*9+i)
-			fmt.Print(line)
+				line := picasso.ReadLine(file, 2+(int(currentLetter)-32)*9+i)
+				fmt.Print(line)
+			}
+			fmt.Println()
 		}
-		fmt.Println()
-	}
 	}
 }
-
--- specificLine.go
+++ specificLine.go
@@ -1,19 +1,19 @@
 package picasso
 
 import (
-    "bufio"
-    "io"
+	"bufio"
+	"io"
 )
 
 func ReadLine(theFile io.Reader, lineNum int) string {
-    sc := bufio.NewScanner(theFile)
+	sc := bufio.NewScanner(theFile)
 	currentLine := 0
-    for sc.Scan() {
-        currentLine++
-        if currentLine == lineNum {
-            return sc.Text()
-        }
-		
-    }
-    return ""
+	for sc.Scan() {
+		currentLine++
+		if currentLine == lineNum {
+			return sc.Text()
+		}
+
+	}
+	return ""
 }
